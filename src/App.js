c463b029b629151f8024e473baa3e5f85c366850
alihassan786 <hassan1233431@gmail.com>
2023-11-28 19:39:45 +0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,76 +1,39 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { NFTStorage, File } from "nft.storage";
 import { Buffer } from "buffer";
 import { ethers } from "ethers";
 import axios from "axios";
-
-// Components
 import Spinner from "react-bootstrap/Spinner";
 import Navigation from "./components/Navigation";
-
-// ABIs
 import NFT from "./abis/NFT.json";
-
-// Config
 import config from "./config.json";
 
 function App() {
   const [provider, setProvider] = useState(null);
   const [account, setAccount] = useState(null);
   const [nft, setNFT] = useState(null);
-
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [image, setImage] = useState(null);
   const [url, setURL] = useState(null);
-
   const [message, setMessage] = useState("");
   const [isWaiting, setIsWaiting] = useState(false);
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     setProvider(provider);
-
     const network = await provider.getNetwork();
-
     const nft = new ethers.Contract(
-      config[network.chainId].nft.address,
+      "0xbB45f9a922D91CEB619e7A119ABE197AAa840558",
       NFT,
       provider
     );
     setNFT(nft);
   };
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
-
-    if (name === "" || description === "") {
-      window.alert("Please provide a name and description");
-      return;
-    }
-
-    setIsWaiting(true);
-
-    // Call AI API to generate a image based on description
-    const imageData = await createImage();
-
-    // Upload image to IPFS (NFT.Storage)
-    const url = await uploadImage(imageData);
-
-    // Mint NFT
-    await mintImage(url);
-
-    setIsWaiting(false);
-    setMessage("");
-  };
-
   const createImage = async () => {
     setMessage("Generating Image...");
-
-    // You can replace this with different model API's
     const URL = `https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2`;
-
-    // Send the request
     const response = await axios({
       url: URL,
       method: "POST",
@@ -85,49 +48,87 @@ function App() {
       }),
       responseType: "arraybuffer",
     });
-
     const type = response.headers["content-type"];
     const data = response.data;
-
     const base64data = Buffer.from(data).toString("base64");
-    const img = `data:${type};base64,` + base64data; // <-- This is so we can render it on the page
+    const img = `data:${type};base64,` + base64data;
     setImage(img);
-
     return data;
   };
 
   const uploadImage = async (imageData) => {
     setMessage("Uploading Image...");
-
-    // Create instance to NFT.Storage
     const nftstorage = new NFTStorage({
       token: process.env.REACT_APP_NFT_STORAGE_API_KEY,
     });
-
-    // Send request to store image
     const { ipnft } = await nftstorage.store({
       image: new File([imageData], "image.jpeg", { type: "image/jpeg" }),
       name: name,
       description: description,
     });
-
-    // Save the URL
     const url = `https://ipfs.io/ipfs/${ipnft}/metadata.json`;
     setURL(url);
-
     return url;
   };
 
   const mintImage = async (tokenURI) => {
     setMessage("Waiting for Mint...");
-
     const signer = await provider.getSigner();
+    const transaction = await nft.connect(signer).mint(tokenURI);
+    const receipt = await transaction.wait();
+    const tokenId = receipt.events[0].args.tokenId.toNumber();
+    return tokenId;
+  };
 
-    const transaction = await nft.connect(signer).mint(tokenURI, {
-      value: ethers.utils.parseUnits("1", "ether"),
-    });
+  const addTokenToWallet = async (
+    tokenAddress,
+    tokenSymbol,
+    tokenDecimals,
+    tokenImage,
+    tokenId
+  ) => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({
+          method: "wallet_watchAsset",
+          params: {
+            type: "ERC721",
+            options: {
+              address: tokenAddress,
+              symbol: tokenSymbol,
+              decimals: tokenDecimals,
+              image: tokenImage,
+            },
+          },
+        });
+      } catch (error) {
+        console.error("Error adding token to wallet:", error);
+      }
+    } else {
+      console.error("MetaMask not detected");
+    }
+  };
 
-    await transaction.wait();
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (name === "" || description === "") {
+      window.alert("Please provide a name and description");
+      return;
+    }
+    setIsWaiting(true);
+    const imageData = await createImage();
+    const url = await uploadImage(imageData);
+    const tokenId = await mintImage(url);
+
+    addTokenToWallet(
+      "0xbB45f9a922D91CEB619e7A119ABE197AAa840558",
+      "NFT",
+      0,
+      url,
+      tokenId
+    );
+    setIsWaiting(false);
+    setMessage("");
   };
 
   useEffect(() => {
@@ -137,7 +138,6 @@ function App() {
   return (
     <div>
       <Navigation account={account} setAccount={setAccount} />
-
       <div className="form">
         <form onSubmit={submitHandler}>
           <input
@@ -154,7 +154,6 @@ function App() {
           />
           <input type="submit" value="Create & Mint" />
         </form>
-
         <div className="image">
           {!isWaiting && image ? (
             <img src={image} alt="AI generated image" />
@@ -168,7 +167,6 @@ function App() {
           )}
         </div>
       </div>
-
       {!isWaiting && url && (
         <p>
           View&nbsp;
